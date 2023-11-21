b8531c9b85219c1970c5ff3ab968c661e5c22e49
alexelwood14 <alex.elwood14@outlook.com>
2023-11-21 14:37:52 +0000

    Fixed bugs

--- src/utils/create_test_val.py
+++ src/utils/create_test_val.py
@@ -24,6 +24,6 @@ train_labels = old_train_labels[is_in_set_c == False]
 val_labels = old_train_labels[is_in_set_c]
 val_labels["file_path"] = val_labels["file_path"].map(replace_train)
 
-pd.to_pickle(val_labels, os.path.join(args.annotations_path, 'new_train_labels.pkl'))
-pd.to_pickle(train_labels, os.path.join(args.annotations_path, 'new_val_labels.pkl'))
+pd.to_pickle(train_labels, os.path.join(args.annotations_path, 'new_train_labels.pkl'))
+pd.to_pickle(val_labels, os.path.join(args.annotations_path, 'new_val_labels.pkl'))
 
