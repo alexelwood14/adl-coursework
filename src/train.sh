abac7fa854b9637a20e4f6fd0e6fa76eecfa8b70
alexelwood14 <alex.elwood14@outlook.com>
2023-11-22 10:45:46 +0000

    Fixed sequential mistake

--- src/train.sh
+++ src/train.sh
@@ -7,8 +7,8 @@
 #SBATCH -e ./log_%j.err # STDERR out
 #SBATCH --gres=gpu:1
 #SBATCH --account=coms030144
-#SBATCH --time=0:20:00
-#SBATCH --mem=16GB
+#SBATCH --time=0:30:00
+#SBATCH --mem=8GB
 #SBATCH --mail-type=ALL # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=wh20899@bristol.ac.uk # Email to which notifications will be sent
 
@@ -18,12 +18,12 @@ module purge
 module load "languages/anaconda3/2021-3.8.8-cuda-11.1-pytorch"
 
 python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0.99 --learning-rate=0.01
-python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0.97 --learning-rate=0.01
-python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0.95 --learning-rate=0.01
-python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0.90 --learning-rate=0.01
-python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0 --learning-rate=0.01
-python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0.99 --learning-rate=0.02
-python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0.97 --learning-rate=0.02
-python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0.95 --learning-rate=0.02
-python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0.90 --learning-rate=0.02
-python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0 --learning-rate=0.02
+# python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0.97 --learning-rate=0.01
+# python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0.95 --learning-rate=0.01
+# python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0.90 --learning-rate=0.01
+# python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0 --learning-rate=0.01
+# python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0.99 --learning-rate=0.02
+# python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0.97 --learning-rate=0.02
+# python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0.95 --learning-rate=0.02
+# python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0.90 --learning-rate=0.02
+# python runner.py --dataset-root="/mnt/storage/scratch/$USER/MagnaTagATune" --epochs=20 --val-frequency=5 --momentum=0 --learning-rate=0.02
